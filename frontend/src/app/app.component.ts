f4e7857f5b3c36355c75adb425e1d86e058b76a5
BrendoXR <114269551+BrendoXR@users.noreply.github.com>
2025-08-19 10:14:08 -0300

    corrigindo imports

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet // necessário para renderizar as rotas
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'] // opcional
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
-
+export class AppComponent {
+  title = 'front_SiteCasaDeLeiro';
+}
